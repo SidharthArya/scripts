6038fd6d3a45aac216166f47873e1ec9975f514e
Sidharth Arya <sidhartharya10@gmail.com>
2020-09-16 04:03:48 +0530

    Auto Commit: Wed Sep 16 04:03:48 IST 2020

--- sxhkd/unminimize.sh
+++ sxhkd/unminimize.sh
@@ -5,5 +5,5 @@ do
 	NAME="$(xprop -id $id | grep ^WM_NAME | awk -F\= '{print $NF}' | tr -d \")"
 	CLASS="$(xprop -id $id | grep ^WM_CLASS | awk -F\= '{print $NF}' | tr -d \")"
 	echo $id $CLASS $NAME
-done | rofi -dmenu | awk '{print $1}' | xargs -I{} bspc node {} -g hidden -f
+done | dmenu | awk '{print $1}' | xargs -I{} bspc node {} -g hidden -f
 
