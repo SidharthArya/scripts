f8ea3fc9cf4facfaef6e2e286c672eb841bb7a60
Sidharth Arya <sidhartharya10@gmail.com>
2020-09-17 02:23:50 +0530

    Auto Commit: Thu Sep 17 02:23:50 IST 2020

--- sxhkd/windows.sh
+++ sxhkd/windows.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-wmctrl -l | awk '{$3="";print}' | rofi -dmenu | awk '{print $1}' | xargs -I{} bspc node {} -g hidden=off -f
+wmctrl -l | awk '{$3="";print}' | dmenu | awk '{print $1}' | xargs -I{} bspc node {} -g hidden=off -f
