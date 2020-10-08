76e7ba00e6631c2d35317ddeee31a50427827cf9
Sidharth Arya <sidhartharya10@gmail.com>
2020-10-08 22:36:18 +0530

    Auto Commit: Thu Oct  8 10:36:18 PM IST 2020

--- sxhkd/file_manager.sh
+++ sxhkd/file_manager.sh
@@ -4,4 +4,5 @@ export NNN_SSHFS="sshfs -o follow_symlinks"        # make sshfs follow symlinks 
 export NNN_COLORS="2136"                           # use a different color for each context
 export NNN_TRASH=1                                 # trash (needs trash-cli) instead of delete
 export EDITOR=nvim
+export NNN_PLUG='f:finder;o:fzopen;p:mocplay;d:diffs;t:nmount'
 nnn
