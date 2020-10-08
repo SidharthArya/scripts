85502dfe20f27cda5539ef40524c1dcbe8184758
Sidharth Arya <sidhartharya10@gmail.com>
2020-10-08 23:41:53 +0530

    Auto Commit: Thu Oct  8 11:41:53 PM IST 2020

--- scripts/fan_regulate.sh
+++ scripts/fan_regulate.sh
@@ -1,22 +1,35 @@
 #!/bin/sh
 
+
+
+
 TEMPARATURES="$( sensors | grep °C | sed 's/.*://g' | awk '{print $1}'| tr -d C°+ | sed 's/\..*//g')"
 MAX=0
+
+
 for i in $TEMPARATURES;
 do
 	MAX=$(( $MAX < $i ? $i : $MAX ))
 done
+
+CURRENT_LEVEL="$(cat /proc/acpi/ibm/fan | tail -n 4 | head -n 1 | awk '{print $2}')"
+
 if (( $MAX > 50 ));
 then 
-	echo level full-speed > /proc/acpi/ibm/fan
+	LEVEL="disengaged"
 elif (( $MAX > 45 ));
 then
-	echo level 7 > /proc/acpi/ibm/fan
+	LEVEL="7"
 elif (( $MAX > 40 ));
 then
-	echo level 4 > /proc/acpi/ibm/fan
+	LEVEL="4"
 else
-	echo level 0 > /proc/acpi/ibm/fan
+	LEVEL="0"
+fi
+if (( $LEVEL != $CURRENT_LEVEL ))
+then
+	echo level "$LEVEL" > /proc/acpi/ibm/fan
+else
+cat /proc/acpi/ibm/fan >> /tmp/fan_status
 fi
 cat /proc/acpi/ibm/thermal >> /tmp/fan_status
-cat /proc/acpi/ibm/fan >> /tmp/fan_status
