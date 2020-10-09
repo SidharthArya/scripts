0a1962af13429b3d76b4b5e2a99290e0b5a72a0a
Sidharth Arya <sidhartharya10@gmail.com>
2020-10-09 21:25:16 +0530

    Auto Commit: Fri Oct  9 09:25:16 PM IST 2020

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -1,6 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 sh ~/.local/scripts/repository.sh
 CURRENT_DIR="$(pwd)"
-cd "$(cat ~/Documents/Sync/drive)";
-drive push *;
-cd "$CURRENT_DIR"
+DRIVE_VALUES="$(cat ~/Documents/Sync/drive)"
+for i in "${DRIVE_VALUES}";
+do
+    pushd "${DRIVE_VALUES%%:*}";
+    FILES="${DRIVE_VALUES##*:}"
+    drive push ${FILES//;/ }
+    popd
+done
