bbde32ef8685a77c5e5b979f0415bfe20f92d59e
Sidharth Arya <sidhartharya10@gmail.com>
2020-11-21 17:46:21 +0530

    Auto Commit: Sat Nov 21 05:46:20 PM IST 2020

--- sxhkd/bookmarks.sh
+++ sxhkd/bookmarks.sh
@@ -3,8 +3,7 @@ browser="surf"
 ROFI="rofi"
 ROFIARGS=" -dmenu -theme Pop-Dark -font 'Noto Sans 14' "
 export IFS=$'\n'
-arg2=$(/usr/bin/ls /home/arya/Documents/Org/Brain/Learning | rofi -dmenu -theme Pop-Dark -font 'Noto Sans 14' -pSubject)
-
+arg2=$(/usr/bin/ls -d /home/arya/Documents/Org/Brain/Learning/*/ | rofi -dmenu -theme Pop-Dark -font 'Noto Sans 14' -pSubject)
 LINK=$(fd .org /home/arya/Documents/Org/Brain/Learning/$arg2 | xargs rg -I "http"  | sed 's/^\s*+\s*//g' | rofi -dmenu -theme Pop-Dark -font 'Noto Sans 14' -p "bookmark")
 if ! [ -z "$LINK" ];
 then
