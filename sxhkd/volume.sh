27ffab45fcd2562d3115fef4e0930e8209c2bb80
Sidharth Arya <sidhartharya10@gmail.com>
2021-01-02 00:57:00 +0530

    Auto Commit: Sat Jan  2 12:56:57 AM IST 2021

--- sxhkd/volume.sh
+++ sxhkd/volume.sh
@@ -5,14 +5,15 @@ case "$1" in
     "increase")
         if  [ $VOLUME -lt 100 ];
         then
-            pactl -- set-sink-volume 0 +5%;
+            pactl -- set-sink-volume 0 +2%;
         fi
         ;;
     "decrease")
         if [ $VOLUME -gt 0 ];
         then
-            pactl -- set-sink-volume 0 -5%;
+            pactl -- set-sink-volume 0 -2%;
         fi
         ;;
 esac
 
+echo 1 > /tmp/bspwm_brightness_volume
