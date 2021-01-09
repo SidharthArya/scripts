44968f8a48bce356e1e56ec2748a1c9b305441fc
Sidharth Arya <sidhartharya10@gmail.com>
2021-01-09 15:25:30 +0530

    Auto Commit: Sat Jan  9 03:25:30 PM IST 2021

--- scripts/fan_regulate.sh
+++ scripts/fan_regulate.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env sh
-mkfifo /tmp/fan_status
+touch /tmp/fan_status
+echo level disengaged > /proc/acpi/ibm/fan
 while true;
 do
-TEMPARATURES="$(sensors | grep °C | grep -v temp11 | sed 's/.*://g' | awk '{print $1}'| tr -d C°+ | sed 's/\..*//g' | tr -d '(A..z')"
-MAX=0
-for i in $TEMPARATURES;
-do
-	   MAX="$(( $MAX < $i ? $i : $MAX ))"
-done
+# TEMPARATURES="$(sensors | grep °C | grep -v temp11 | sed 's/.*://g' | awk '{print $1}'| tr -d C°+ | sed 's/\..*//g' | tr -d '(A..z')"
+# MAX=0
+# for i in $TEMPARATURES;
+# do
+	   # MAX="$(( $MAX < $i ? $i : $MAX ))"
+# done
+MAX=$(acpi -t  | awk '{print $4}' | sed 's/\..*//g')
 CURRENT_LEVEL="$(cat /proc/acpi/ibm/fan | tail -n 4 | head -n 1 | awk '{print $2}')"
 if (( $MAX > 50 ));
 then 
@@ -21,13 +23,13 @@ then
 else
 	LEVEL="0"
 fi
-if (( $LEVEL != $CURRENT_LEVEL ))
+if [[ $LEVEL != $CURRENT_LEVEL ]]
 then
 	echo level "$LEVEL" > /proc/acpi/ibm/fan
 	cat /proc/acpi/ibm/fan >> /tmp/fan_status
 else
 	cat /proc/acpi/ibm/fan >> /tmp/fan_status
 fi
-echo $MAX >> /tmp/fan_status
+echo $MAX > /tmp/fan_status
 sleep 15
 done
--- sxhkd/calculator.py
+++ sxhkd/calculator.py
@@ -1,6 +1,5 @@
 import sympy as sym
 import numpy as np
 import pandas as pd
-import dbus
 import sys
 
