d9152bd5b1417a926442b9485cbaac4ad5f537d4
Sidharth Arya <sidhartharya10@gmail.com>
2021-02-03 07:18:19 +0530

    Auto Commit: Wed Feb  3 07:18:19 AM IST 2021

--- sxhkd/calculator.py
+++ sxhkd/calculator.py
@@ -1,5 +1,6 @@
 import sympy as sym
 import numpy as np
 import pandas as pd
+import scipy as sp
 import sys
 
--- sxhkd/session_start.sh
+++ sxhkd/session_start.sh
@@ -1,12 +1,7 @@
 #!/bin/sh
-gocryptfs -extpass 'zenity --password Files' ~/.drive/Personal ~/Documents  &&
-    (
-    emacs --config tracking & 
-    emacs --config org &
-    st -e sh ~/.local/scripts/sync.sh &
-    sxiv ~/Documents/Org/Tracking/images &
-    python ~/.local/scripts/electricitybill.py &
-    ) ||
-        (
-            kdialog --msgbox "Hello Guest";
-    )
+emacs --config tracking & 
+emacs --config org &
+st -e sh ~/.local/scripts/sync.sh &
+sxiv ~/Documents/Org/Tracking/images &
+python ~/.local/scripts/electricitybill.py &
+if ! pgrep urxvtd; then urxvtd -q -o -f; fi
