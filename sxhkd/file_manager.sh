afff5635b561688564fb6cfd9c712992d9845bce
Sidharth Arya <sidhartharya10@gmail.com>
2021-03-01 05:18:00 +0530

    Auto Commit: Mon Mar  1 05:17:59 AM IST 2021

--- sxhkd/file_manager.sh
+++ sxhkd/file_manager.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-export NNN_BMS='d:~/Documents;u:/home/user/Cam Uploads;D:~/Downloads/;m:/run/media/arya;g:/run/user/1000/gvfs;t:~/.local/share/Trash/files;s:~/Pictures/Screenshots'
+export NNN_BMS='D:~/Documents;l:/home/arya/Private/Lectures/;d:~/Downloads/;m:/run/media/arya;g:/run/user/1000/gvfs;t:~/.local/share/Trash/files;s:~/Pictures/Screenshots'
 export NNN_SSHFS="sshfs -o follow_symlinks"        # make sshfs follow symlinks on the remote
 export NNN_COLORS="2136"                           # use a different color for each context
-#export NNN_TRASH=1                                 # trash (needs trash-cli) instead of delete
+# export NNN_TRASH=1                                 # trash (needs trash-cli) instead of delete
 export EDITOR=nvim
 export NNN_PLUG='m:mount;p:preview-tabbed;d:dragdrop'
 export NNN_FIFO=/tmp/nnn.fifo
---
+++ sxhkd/nnn.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+source ~/.config/sxhkd/scripts/file_manager.sh
+nnn -x
