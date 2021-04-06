f407ef659f4fb188d5dec30a938bf37f2025e02d
Sidharth Arya <sidhartharya10@gmail.com>
2021-04-06 08:06:58 +0530

    Auto Commit: Tue Apr  6 08:06:58 AM IST 2021

--- sxhkd/volume.sh
+++ sxhkd/volume.sh
@@ -1,17 +1,18 @@
 #!/bin/sh
 VOLUME="$(pamixer --get-volume)"
+
 echo $VOLUME
 case "$1" in
     "increase")
         if  [ $VOLUME -lt 100 ];
         then
-            pactl -- set-sink-volume 0 +2%;
+            pactl -- set-sink-volume 0 +2% && canberra-gtk-play -i audio-volume-change;
         fi
         ;;
     "decrease")
         if [ $VOLUME -gt 0 ];
         then
-            pactl -- set-sink-volume 0 -2%;
+            pactl -- set-sink-volume 0 -2% && canberra-gtk-play -i audio-volume-change;
         fi
         ;;
 esac
