8da2345fc8892a83fc2e37d5bc16ca3aeb961c09
Sidharth Arya <sidhartharya10@gmail.com>
2021-05-30 21:55:12 +0530

    Auto Commit: Sun May 30 09:55:12 PM IST 2021

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -1,5 +1,7 @@
-#!/bin/sh
+#!/bin/bash
+source ~/Documents/Org/Scripts/personal.sh
 sh ~/.local/scripts/repository.sh
+echo $GIT_ASKPASS
 CURRENT_DIR="$(pwd)"
 cd "$(cat ~/Documents/Sync/drive)";
 drive push *;
