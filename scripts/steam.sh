0aaa8a3ea645f3c64c75f8e24ca2c02ff80fbd0c
Sidharth Arya <sidhartharya10@gmail.com>
2022-03-21 10:21:53 +0530

    Auto Commit: Mon Mar 21 10:21:53 AM IST 2022

--- scripts/steam.sh
+++ scripts/steam.sh
@@ -9,6 +9,7 @@ docker run \
        -e PULSE_SERVER=unix:${XDG_RUNTIME_DIR}/pulse/native \
        -v /tmp/.X11-unix:/tmp/.X11-unix \
        -v /dev/shm:/dev/shm \
+       -v /dev/bus/usb:/dev/bus/usb \
        -v ${XDG_RUNTIME_DIR}/pulse:${XDG_RUNTIME_DIR}/pulse \
        -v /home/arya/.local/share/Steam:/home/steam/.local/share/Steam \
        tianon/steam || docker start steam
