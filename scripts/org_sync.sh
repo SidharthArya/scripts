8e1fb1b84b882cb4ee4172ec53201b3dc8cab381
Sidharth Arya <sidhartharya10@gmail.com>
2022-05-30 02:54:12 +0530

    Auto Commit: Mon May 30 02:54:12 AM IST 2022

--- scripts/org_sync.sh
+++ scripts/org_sync.sh
@@ -1,16 +1,20 @@
 #!/usr/bin/env bash
 
-while inotifywait -e close_write *.org --format '%w'| while read FILE
+
+while inotifywait -m -r -e create -e close_write -e moved_to * --format '%w' | while read FILE
     do
-        git add .
-        git stash push -m "tbc"
-        git pull
-        STASH="$(git stash list | grep tbc | head -n 1 | awk '{print $1}' | rev | cut -c 2- | rev)"
-        git stash pop $STASH
-        notify-send "Git" "Committed: $FILE changed"
-	git add $FILE
-	git commit -m "Auto: $FILE changed"
-        git push
+        if [[ $FILE =~ ".org" ]];
+        then
+           git add .
+           git stash push -m "tbc"
+           git pull
+           STASH="$(git stash list | grep tbc | head -n 1 | awk '{print $1}' | rev | cut -c 2- | rev)"
+           git stash pop $STASH
+           notify-send "Git" "Committed: $FILE changed"
+	   git add $FILE
+	   git commit -m "Auto: $FILE changed"
+           git push
+        fi
     done
 do
     echo "Committed to Github";
